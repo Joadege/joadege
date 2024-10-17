7e139d0a90475812e3c9ab55eb0e5a860b273092
Joadege <jadeghe166@gmail.com>
2024-10-17 17:43:57 -0400

    fixed errors

---
+++ python_1/demo'.py
@@ -0,0 +1,21 @@
+username = 'joadege'
+nickname = "Jo adege. embrace the knowledge."
+age = 17
+bankAccountValue = 25.50
+lotteryamount = 100000
+
+print("Your account balance is" bankAccountValue)
+print("joadege" , username)
+
+# for loops
+number_of_times=5
+#plain english
+
+for index in range(5):
+    #print('hello world')
+    Print(index)
+    
+    #while cond <= 5:
+    print('hello world')
+cond = cond + 1
+print(cond)
---
+++ python_1/demo.py
@@ -0,0 +1,23 @@
+username = 'joadege'
+nickname = "Jo adege. embrace the knowledge."
+age = 17
+bankAccountValue = 25.50
+lotteryamount = 100000
+
+print("Your account balance is",bankAccountValue)
+print("joadege" , username)
+
+# for loops
+number_of_times=5
+#plain english
+
+for index in range(5):
+    #print('hello world')
+    print(index)
+
+
+cond = 0 
+while cond <5:
+    print('hello world')
+    cond = cond + 1
+    print(cond)
