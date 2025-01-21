a90aa471f505d9a1ba8d00f06ca1e1b8975ec242
Your Name <your.email@example.com>
2025-01-21 18:07:13 -0500

    pushing new c sharp

---
+++ c#_1/demo2.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demo2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string myname = "Player";
+    Console.WriteLine(myname);
+    
+
+    int Add(int a, int b)
+    
+    return a + b;
+    
+
+    int result = Add(8, 8);
+//return result
+
+
+Console.WriteLine(result);
+    void Start() {
+   
+        
+       
+        Debug.Log(myDoubleNum);
+       
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    
+}
