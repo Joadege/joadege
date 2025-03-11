3015bc3dee1b6c7f9a5f8a7a4a13abff8745218e
Your Name <your.email@example.com>
2025-03-11 18:27:42 -0400

    Pushing new speedboost timer changes

--- 2d_game_mechanics_1/everlasting-candy-KJ/Script/speedboost_pickup.gd
+++ 2d_game_mechanics_1/everlasting-candy-KJ/Script/speedboost_pickup.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 
 func _on_timer_timeout() -> void:
-	print("game") # Replace with function body.
+	print("game") # Replace with function body.$GameTimer.start(5)  # Start the timer for 5 seconds
+top the timer if needed
